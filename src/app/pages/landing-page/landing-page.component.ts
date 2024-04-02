d73d7a67a42ef24d198d8ac45b358a9a8e064c52
Santiago Benitez <santiago9607b@gmail.com>
2024-04-02 09:18:43 -0500

    Update logo and text

--- src/app/pages/landing-page/landing-page.component.ts
+++ src/app/pages/landing-page/landing-page.component.ts
@@ -9,25 +9,25 @@ import { TableroModel } from 'src/app/models/tablero.model';
 export class LandingPageComponent implements OnInit {
   tableros: TableroModel[] = [
     new TableroModel(
-      "Dirección de TIC's y gobierno digital",
+      "Gerencia Estratégica de TIC",
       'Información pública de la Secretaría de Protección Social, casos de violencia',
       'Para cada barrio de Tunja encuentra el resumen estadístico de los casos de violencia para los dos ultimos años (2020,2021)',
       '/tableros/proteccion-social/casos-violencia'
     ),
     new TableroModel(
-      "Dirección de TIC's y gobierno digital",
+      "Gerencia Estratégica de TIC",
       'Información pública de la Secretaría de Protección Social, intentos de suicidio',
       'Encuentra el resumen estadístico de los intentos de suicidio para los dos ultimos años (2020,2021) en la ciudad de Tunja',
       '/tableros/proteccion-social/intentos-suicidio'
     ),
     new TableroModel(
-      "Dirección de TIC's y gobierno digital",
+      "Gerencia Estratégica de TIC",
       'Información pública Instalaciones de Internet - La Conectividad nos Une',
       'Del programa de la Alcaldia de Tunja, la conectividad nos une, se muestran las estadisticas para instalaciones del año 2022',
       '/tableros/direccion-tics/instalaciones-internet'
     ),
     new TableroModel(
-      "Dirección de TIC's y gobierno digital",
+      "Gerencia Estratégica de TIC",
       'Información pública Usuarios Conectados en Zonas Wifi',
       'Del programa de la Alcaldia de Tunja, la conectividad nos une, se muestran las estadisticas para conexiones en zonas Wi-Fi del año 2021 al 2022',
       '/tableros/direccion-tics/zonas-wifi'
